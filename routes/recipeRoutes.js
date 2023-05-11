192b3fe60ce048d2361917702f010f02aa000d49
vincenzochiovaro <vincenzochiovaro194@gmail.com>
2023-05-11 08:50:16 +0100

    add recipeByRecipeName endpoint

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,10 +1,15 @@
 require("../envRateLimiter");
 
 const recipeRouter = require("express").Router();
-const { getRandomRecipe, postRecipe } = require("../controller");
+const {
+  getRandomRecipe,
+  postRecipe,
+  getRecipeByRecipeName,
+} = require("../controller");
 
 const limiter = require("../rateLimit");
 recipeRouter.get("/random", getRandomRecipe);
+recipeRouter.get("/:recipeName", getRecipeByRecipeName);
 recipeRouter.post("/", limiter, postRecipe);
 
 module.exports = recipeRouter;
